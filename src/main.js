b29f5ee9ed78bb4d2f47ab25849df2508295a308
Tommy Alhamra <tommy.alhamra@gmail.com>
2021-01-05 19:01:54 +0700

    end githubpage , welcome vercel

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import './assets/style/main.css'
+import router from './router' 
+import '@/assets/style/main.css'
 
 Vue.config.productionTip = false
 
--- vue.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  publicPath: '/imotD.github.io/'    
-}
