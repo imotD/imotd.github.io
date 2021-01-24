519fc66df75f97d6588c5c3cc88aff9605fbccb0
Tommy Alhamra <tommy.alhamra@gmail.com>
2021-01-24 21:47:22 +0700

    add card in project

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -8,24 +8,44 @@
         excepturi nulla rem! Corporis non ullam dolore culpa dolores amet.
       </span>
     </div>
-    <main>
-      <router-link>
-        <div>
-          tes
+    <main class="mb-20">
+      <div
+        class="grid grid-cols-12 border-2 hover:border-blue-500 hover:shadow-lg rounded-md"
+      >
+        <div class="col-span-5 p-5">
+          <p class="text-gray-500 tracking-wider">Proyek Pribadi</p>
+          <h3 class="font-black text-2xl tracking-wider leading-10">
+            Git Master
+          </h3>
+          <span class="font-light">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+            distinctio quae dicta incidunt sunt quos rem iure impedit non vel?
+          </span>
+          <div></div>
+          <router-link to="/test">
+            Selengkapnya
+          </router-link>
         </div>
-      </router-link>
-      <div class="grid gap-3 grid-12">
-        <div class="border-2">1</div>
-        <div class="border-2">2</div>
-        <div class="border-2">3</div>
-        <div class="border-2">4</div>
+        <div class="col-span-7 bg-blue-100 rounded-tr-lg">
+          <img class="h-36 mt-14 w-4/5 bg-green-800 m-auto" src="" alt="img" />
+        </div>
       </div>
     </main>
+    <Footer />
   </div>
 </template>
 
 <script>
-export default {};
+import Footer from "@/components/Footer.vue";
+export default {
+  name: "Project",
+  components: {
+    Footer,
+  },
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
