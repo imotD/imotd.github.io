cf2f4d7a06d78822a3b9d37742ad60744874fef4
Tommy Alhamra <tommy.alhamra@gmail.com>
2021-02-07 19:03:52 +0700

    add comp icon

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :class="nav">
+    <button
+      class="p-2 bg-gray-100 rounded text-gray-300 hover:text-black"
+      :title="title"
+      @click="$emit('btn')"
+      :to="link"
+    >
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseIcon",
+  props: ["nav", "title", "link"],
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,68 +1,81 @@
 <template>
-  <div class="container mx-auto px-32 pt-12">
-    <div class="pb-10">
-      <h1 class="text-5xl font-black tracking-wide">Portofolio</h1>
-      <span class="font-light tracking-wide">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi,
-        adipisci, facere cum error eum magni neque rerum dolores culpa, nihil
-        excepturi nulla rem! Corporis non ullam dolore culpa dolores amet.
-      </span>
-    </div>
-    <main class="mb-20">
-      <div
-        class="grid grid-cols-12 border-2 hover:border-blue-500 hover:shadow-lg rounded-md h-80 relative"
-      >
-        <div class="col-span-5 p-5">
-          <p class="text-gray-500 tracking-wider">Proyek Pribadi</p>
-          <h3 class="font-black text-2xl tracking-wider leading-loose">
-            Git Master
-          </h3>
-          <div class="font-light line-clamp overflow-hidden">
-            Vue.js adalah kerangka kerja JavaScript yang bersifat progresif,
-            bersumber terbuka untuk membangun antarmuka pengguna. Integrasi ke
-            dalam proyek yang menggunakan pustaka JavaScript lainnya mudah
-            dilakukan dengan Vue karena ia dirancang untuk bisa berjalan hanya
-            di beberapa bagian halaman web
-          </div>
-          <div class="flex flex-row gap-2 mt-3">
-            <div
-              class="h-11 w-11 bg-gray-100 rounded flex justify-items-center items-center"
-              title="vue"
-            >
-              <img
-                class="m-auto"
-                src="@/assets/img/logo/logo-vuejs.png"
-                alt="tech"
-              />
+  <div class="project">
+    <div class="container mx-auto px-32 pt-12">
+      <div class="pb-10">
+        <h1 class="text-5xl font-black tracking-wide">Portofolio</h1>
+        <span class="font-light tracking-wide">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi,
+          adipisci, facere cum error eum magni neque rerum dolores culpa, nihil
+          excepturi nulla rem! Corporis non ullam dolore culpa dolores amet.
+        </span>
+      </div>
+      <main class="mb-20">
+        <div
+          class="grid grid-cols-12 border-2 hover:border-blue-500 hover:shadow-lg rounded-md h-80 relative"
+        >
+          <div class="col-span-5 p-5">
+            <p class="text-gray-500 tracking-wider">Proyek Pribadi</p>
+            <h3 class="font-black text-2xl tracking-wider leading-loose">
+              Git Master
+            </h3>
+            <div class="font-light line-clamp overflow-hidden">
+              Vue.js adalah kerangka kerja JavaScript yang bersifat progresif,
+              bersumber terbuka untuk membangun antarmuka pengguna. Integrasi ke
+              dalam proyek yang menggunakan pustaka JavaScript lainnya mudah
+              dilakukan dengan Vue karena ia dirancang untuk bisa berjalan hanya
+              di beberapa bagian halaman web
+            </div>
+            <div class="flex flex-row gap-2 mt-3">
+              <div
+                class="h-11 w-11 bg-gray-100 rounded flex justify-items-center items-center"
+                title="vue"
+              >
+                <img
+                  class="m-auto"
+                  src="@/assets/img/logo/logo-vuejs.png"
+                  alt="tech"
+                />
+              </div>
+            </div>
+            <div class="absolute bottom-0 p-2 pb-5">
+              <router-link to="/test" class="flex gap-5 hover:text-blue-500">
+                Selengkapnya <ph-arrow-right :size="22" />
+              </router-link>
             </div>
           </div>
-          <div class="absolute bottom-0 p-2 pb-5">
-            <router-link to="/test" class="flex gap-5 hover:text-blue-500">
-              Selengkapnya <ph-arrow-right :size="22" />
-            </router-link>
+          <div class="col-span-7 bg-blue-100 rounded-tr-lg relative">
+            <div
+              class="absolute bottom-0 m-auto left-0 right-0 h-60 mt-14 w-4/5 bg-cover bg-center m-auto bg-yellow-100 rounded-t"
+              :style="bgProject"
+            ></div>
+            <!-- <img class="h-36 mt-14 w-4/5 bg-green-800 m-auto" src="" alt="img" /> -->
           </div>
         </div>
-        <div class="col-span-7 bg-blue-100 rounded-tr-lg relative">
-          <div
-            class="absolute bottom-0 m-auto left-0 right-0 h-60 mt-14 w-4/5 bg-cover bg-center m-auto bg-yellow-100 rounded-t"
-            :style="bgProject"
-          ></div>
-          <!-- <img class="h-36 mt-14 w-4/5 bg-green-800 m-auto" src="" alt="img" /> -->
-        </div>
-      </div>
-    </main>
+      </main>
+      <BaseIcon nav="icons icons--left" title="Back" @btn="back()">
+        <ph-arrow-u-up-left :size="35" />
+      </BaseIcon>
+
+      <BaseIcon nav="icons icons--right" title="Karya" @btn="next()">
+        <ph-arrow-u-down-right :size="35" />
+      </BaseIcon>
+    </div>
     <Footer />
   </div>
 </template>
 
 <script>
-import { PhArrowRight } from "phosphor-vue";
+import BaseIcon from "@/components/BaseIcon.vue";
+import { PhArrowRight, PhArrowUUpLeft, PhArrowUDownRight } from "phosphor-vue";
 import Footer from "@/components/Footer.vue";
 export default {
   name: "Project",
   components: {
+    BaseIcon,
     Footer,
     PhArrowRight,
+    PhArrowUUpLeft,
+    PhArrowUDownRight,
   },
   data() {
     return {
@@ -70,6 +83,14 @@ export default {
         backgroundImage: `url(${require("@/assets/img/project/p-gitmaster.png")})`,
       },
     };
+  },
+  methods: {
+    back() {
+      this.$router.go(-1);
+    },
+    next() {
+      this.$router.push("/karya");
+    },
   },
 };
 </script>
